3b4d33e4638fc5b6702e38b45acf5bbf3a68ee46
Minh Thao Nguyen <62545336+minhthaodev95@users.noreply.github.com>
2021-09-05 13:34:39 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', 'ec2-18-209-153-180.compute-1.amazonaws.com'),
+        host: env('DATABASE_HOST', 'ec2-52-17-1-206.eu-west-1.compute.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'df5lfd9c75nm39'),
-        username: env('DATABASE_USERNAME', 'blqbojmsgxmzad'),
-        password: env('DATABASE_PASSWORD', 'cf8420705ec60eaa7779b3335ad1ad936f113afd41a17c7a342afe820e60e465'),
+        database: env('DATABASE_NAME', 'dcls15l6m6te73'),
+        username: env('DATABASE_USERNAME', 'ishxzgdyjbhhuu'),
+        password: env('DATABASE_PASSWORD', 'f598f2ac30706e3c1bdf45f156924ba601fc2c86c17bdfe0156b3a551084f98e'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
