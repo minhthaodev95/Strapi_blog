e6db665b809dff6deab423cf5a0ff6a1f3c6b60c
Minh Thao Nguyen <62545336+minhthaodev95@users.noreply.github.com>
2021-09-05 13:11:37 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', 'ec2-18-209-153-180.compute-1.amazonaws.com'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi_blog'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'Meadowcool123@'),
+        database: env('DATABASE_NAME', 'df5lfd9c75nm39'),
+        username: env('DATABASE_USERNAME', 'blqbojmsgxmzad'),
+        password: env('DATABASE_PASSWORD', 'cf8420705ec60eaa7779b3335ad1ad936f113afd41a17c7a342afe820e60e465'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
